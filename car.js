86a8a375f4c08b7fdc194d685bdfc25fe055b0f9
Yakau Trafimovich <5243212@gmail.com>
2022-03-25 08:36:10 +0100

    init

---
+++ car.js
@@ -0,0 +1,13 @@
+class Car {
+    constructor(brand, model, yearOfManufacturing, maxSpeed, maxFuelVolume, fuelConsumption, currentFuelVolume, isStarted, mileage) {
+        this.brand = brand;
+        this.model = model;
+        this.yearOfManufacturing = yearOfManufacturing;
+        this.maxSpeed = maxSpeed;
+        this.maxFuelVolume = maxFuelVolume;
+        this.fuelConsumption = fuelConsumption;
+        this.currentFuelVolume = currentFuelVolume;
+        this.isStarted = isStarted;
+        this.mileage = mileage;
+    }
+}
