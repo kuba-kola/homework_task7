4e08ae2168772716da8d908779b088710ccca44a
Yakau Trafimovich <5243212@gmail.com>
2022-03-27 15:10:14 +0200

    add get and set

--- car.js
+++ car.js
@@ -16,11 +16,97 @@ class Car {
     }
 
     set brand(newBrand) {
-        if (typeof newBrand !== 'string' || newBrand === '' || newBrand.length > 50) {
+        const isIncorrect = typeof newBrand !== 'string' || newBrand === '' || newBrand.length > 50;
+        
+        if (isIncorrect) {
             return;
         }
 
         this.brand = newBrand;
+    }
+
+    get model() {
+        return this.model;
+    }
+    
+    set model(newModel) {
+        const isIncorrect = typeof newModel !== 'string' || newModel === '' || newModel.length > 50;
+
+        if (isIncorrect) {
+            return;
+        }
+
+        this.model = newModel;
+    }
+
+    get yearOfManufacturing() {
+        return this.yearOfManufacturing;
+    }
+
+    set yearOfManufacturing(newYearOfManufacturing) {
+        const now = new Date();
+        const currentYear = now.getFullYear();
+        const isIncorrect = !checkValid(newYearOfManufacturing) || newYearOfManufacturing > currentYear || newYearOfManufacturing < 1900;
+
+        if (isIncorrect) {
+            return;
+        };
+
+        this.yearOfManufacturing = newYearOfManufacturing;
+    }
+
+    get maxSpeed() {
+        return this.maxSpeed;
+    }
+
+    set maxSpeed(newMaxSpeed) {
+        const isIncorrect = newMaxSpeed <= 100 || newMaxSpeed >= 300 || !checkValid(newMaxSpeed);
+
+        if (isIncorrect) {
+            return;
+        };
+
+        this.maxSpeed = newMaxSpeed;
+    }
+
+    get maxFuelVolume() {
+        return this.maxFuelVolume;
+    }
+
+    set maxFuelVolume(newMaxFuelVolume) {
+        const isIncorrect = newMaxFuelVolume <= 5 || newMaxFuelVolume >= 20 || !checkValid(newMaxFuelVolume);
+
+        if (!isIncorrect) {
+            return;
+        };
+
+        this.maxFuelVolume = newMaxFuelVolume;
+    }
+
+    get fuelConsumption() {
+        return this.fuelConsumption;
+    }
+
+    set fuelConsumption(newFuelConsumption) {
+        const isIncorrect = newFuelConsumption >= 10 || newFuelConsumption <= 25 || checkValid(newFuelConsumption);
+
+        if (isIncorrect) {
+            return;
+        };
+
+        this.fuelConsumption = newFuelConsumption;
+    }
+
+    get currentFuelVolume() {
+        return this.currentFuelVolume;
+    }
+
+    get isStarted() {
+        return this.isStarted;
+    }
+
+    get mileage() {
+        return this.mileage;
     }
 }
 
